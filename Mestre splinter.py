77698134e3b61e2d9aa895dbf81751aad625d576
JOSE CELSO CARVALHO LEITE <79555971+celsoleite38@users.noreply.github.com>
2022-11-08 00:14:09 -0300

    Add files via upload

---
+++ Mestre splinter.py
@@ -0,0 +1,11 @@
+nome = input ('Qual o seu nome?( leo ou celso): ')
+for item in nome:
+    if nome == 'leo':
+        print ("Faixa branca não mata rato quebra os 4 dedos")
+    else:
+        print ("Deixa o faixa preta matar com o Rodo, sem quebrar nada")
+    if nome == 'celso':
+        print ("A sim, Um faixa preta OSSS!!")
+    else:
+        print ('kOHAI, ,Respeite o mais graduado!!')
+    
